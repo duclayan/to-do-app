90a0ee841a0b68c6a2940a9b15d85d0a5943d7ff
duclayan <duclayandominique@gmail.com>
2020-11-23 15:33:17 +0400

    Create Update

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -12,7 +12,10 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test 'should create' do
-        create_category_url(:name => 'Sampletitle', :description => 'This is a sample body text of an input')
+        post create_category_path, params: { category: {name: 'Sampletitle', description: 'This is a sample body text of an input'} }
+        assert_response :redirect
+
+        follow_redirect!
         assert_response :success
     end
 
