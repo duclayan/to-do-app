b9b36d7d09f3d146cd3b6e487452d2b30b2a5ba7
duclayan <duclayandominique@gmail.com>
2020-11-30 10:29:30 +0400

    Task: Integration Test

--- test/integration/create_task_test.rb
+++ test/integration/create_task_test.rb
@@ -13,7 +13,7 @@ class CreateTaskTest < ActionDispatch::IntegrationTest
     end
 
     test '01: User creates new task' do
-        # 1.Check if new_category_path is working
+        # 1.Check if new_task_path is working
         # 2. IF working => success : fail
         # 3. Check if successfully added (assert_difference)
         # 4. Create task with the input
@@ -23,8 +23,46 @@ class CreateTaskTest < ActionDispatch::IntegrationTest
 
         assert_difference 'Task.count', 1 do 
             post create_task_path, params: { task: {title: 'Create an Amazing API', description: 'Get started by searching in Google'}}
-            assert_response :success
+            assert_response :redirect
         end
 
+        follow_redirect!
+        assert_response :success
     end
+
+    test '02: Update current task' do
+        #1. @task holds fixture
+        #2. Access Edit controller - should pass id parameter
+        #3. Create changes - assert_changes
+        #4. Access Update Method From Controller 
+        
+        @task = tasks(:one)
+        get edit_task_path(:id => @task.id)
+        assert_response :success
+
+        assert_changes '@task.title' do
+            put update_task_path(:id => @task.id), params: {task: {title: 'Create and Amazing API', description: 'Get started with an amazing API by searching'}}
+            @task.reload
+            assert_response :redirect
+        end
+
+        follow_redirect!
+        assert_response :success
+    end
+
+    test '03: Delete task - task is done' do
+        #Same as update
+        
+        @task = tasks(:one)
+        get edit_task_path(:id => @task.id)
+        assert_response :success
+
+        assert_difference 'Task.count', -1 do 
+            delete delete_task_path(:id => @task.id)
+            assert_response :redirect
+        end
+        follow_redirect!
+        assert_response :success
+    end
+
 end
