a66783dc797d6a0939bdbddd7368dde9556f311c
duclayan <duclayandominique@gmail.com>
2020-12-05 05:45:45 +0400

    Improved Routes - remvoed self declared edit_task and update_task

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # resources :tasks
   # resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonr
-  
-  # get 'categories/new' 
-  # get 'tasks/:id/edit' => 'tasks#edit', as: 'edit_task'
-  # patch '/tasks/:id/edit' => 'tasks#update', as: 'update_task'
 
   resources :categories do
     resources :tasks
